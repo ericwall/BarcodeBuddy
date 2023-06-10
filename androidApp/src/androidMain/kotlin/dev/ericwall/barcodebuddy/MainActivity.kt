8f1975aa5fd59f6f652369c898868d06581031b0
ericwall <ericdonaldwall@gmail.com>
2023-06-10 13:15:42 -0500

    move namespace

--- androidApp/src/androidMain/kotlin/dev/ericwall/barcodebuddy/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/dev/ericwall/barcodebuddy/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myapplication
+package dev.ericwall.barcodebuddy
 
 import MainView
 import android.os.Bundle
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "MyApplication"
+rootProject.name = "BarcodeBuddy"
 
 include(":androidApp")
 include(":shared")
