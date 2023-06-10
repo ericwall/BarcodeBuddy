dbce7efdb52e70f033ff4555382f31223b16bacb
ericwall <ericdonaldwall@gmail.com>
2023-06-10 14:30:48 -0500

    Update build.gradle.kts

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -24,7 +24,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "KotlinMultiplatformComposeDesktopApplication"
+            packageName = "Barcode Buddy"
             packageVersion = "1.0.0"
         }
     }
